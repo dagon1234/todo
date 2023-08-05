0a1a60bf9b8b8a922e10c47c09dd4592229528cb
Methinee Phormsorn <dagontunk1234@gmail.com>
2023-08-05 15:57:40 +0700

    todo list

---
+++ src/app/todo-list.ts
@@ -0,0 +1,16 @@
+import { TodoItem } from './todo-item';
+
+export class TodoList {
+  constructor(public user: string, private todoItems: TodoItem[] = []) {
+    // no statements required
+  }
+  get items(): readonly TodoItem[] {
+    return this.todoItems;
+  }
+  addItem(task: string) {
+    this.todoItems.push(new TodoItem(task));
+  }
+}
+
+let list = new TodoList('aa',[])
+list.items
