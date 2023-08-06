9cf712d194e800f2043d69ef8674087f49cafcda
Methinee Phormsorn <dagontunk1234@gmail.com>
2023-08-06 11:24:03 +0700

    showComplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { TodoList } from './todo-list';
 import { TodoItem } from './todo-item';
+import { TodoList } from './todo-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   private list = new TodoList('Bob', [
@@ -13,20 +13,22 @@ export class AppComponent {
     new TodoItem('Get flowers'),
     new TodoItem('Collect tickets'),
   ]);
-  
-  
+
+  showComplete = false;
+
   get username(): string {
     return this.list.user;
   }
-    
+
   get itemCount(): number {
     return this.list.items.filter((item) => !item.complete).length;
   }
 
-  get items(): readonly TodoItem[]{
-    return this.list.items.filter(item => !item.complete);
+  get items(): readonly TodoItem[] {
+    //return this.list.items.filter((item) => !item.complete);
+    return this.list.items.filter(item => this.showComplete || !item.complete);
   }
-  
+
   addItem(newItem: string) {
     if (newItem != '') {
       this.list.addItem(newItem);
